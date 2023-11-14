1bcaf89146e482b2df67b914cca9c3c6acb16dc1
Abhinil-07 <abhinilnath10@gmail.com>
2023-11-15 01:11:51 +0530

    Fixed events page

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -6,9 +6,9 @@ import eventTimeline from "../../assets/event-timeline.png";
 
 const Events = () => {
   return (
-    <div className="events flex justify-center items-center flex-col lg:h-[365vh] h-[310vh]">
+    <div className="events flex justify-center items-center flex-col lg:h-[350vh] h-[550vh]">
       <h1 className="text-center blue-main lg:text-[100px] text-[60px]">Events</h1>
-      <div className="grid lg:grid-cols-3 grid-cols-2 lg:gap-[5rem] gap-[2rem] w-[80%] ">
+      <div className="grid lg:grid-cols-3 grid-cols-1 lg:gap-[5rem] gap-[2rem] w-[80%] ">
         {/* <div className="grid-container"> */}
         <Cards />
         <Cards />
@@ -21,7 +21,7 @@ const Events = () => {
         <Cards />
       </div>
 
-      <div className="flex flex-col mt-[133px] items-center justify-center">
+      <div className="flex flex-col lg:mt-[133px] mt-10 items-center justify-center">
         
           <h1 className="lg:text-[75px] text-[50px] purple-main  text-center">
             Events
@@ -30,7 +30,7 @@ const Events = () => {
             Timeline
           </h1>
        
-        <img className=" mt-4 w-[606px] h-[758px]" src={eventTimeline} alt="" />
+        <img className=" mt-4 lg:w-[606px] w-[330px] lg:h-[758px] " src={eventTimeline} alt="" />
       </div>
     </div>
   );
