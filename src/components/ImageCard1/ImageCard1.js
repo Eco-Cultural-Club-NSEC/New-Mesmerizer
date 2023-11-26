58caca278157b4967c84b833a31d41d4c11d82db
Abhinil Nath <85664966+Abhinil-07@users.noreply.github.com>
2023-11-26 12:17:43 +0530

    Update ImageCard1.js

--- src/components/ImageCard1/ImageCard1.js
+++ src/components/ImageCard1/ImageCard1.js
@@ -18,7 +18,7 @@ function ImageCard1({url}) {
        <img 
         src={url} 
         alt="pic"
-       loading="lazy" *
+       loading="lazy" 
        className="border border-white h-[130px] w-[200px] lg:h-[180px] lg:w-[300px]" 
         onClick={() => { 
           openModal(url); 
